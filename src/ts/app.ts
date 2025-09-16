5541801cc1a5a32d9e9fbcead0f64fb07d58829f
Federico Moretti <hello@federicomoretti.it>
2025-09-16 14:23:19 +0200

    chore: add full Vite support

---
+++ src/ts/app.ts
@@ -0,0 +1,2 @@
+import './../scss/style.scss';
+import './otp-form';
--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,15 @@ export default {
     emptyOutDir: true,
     rolldownOptions: {
       input: {
+        'assets/css/style': 'src/scss/style.scss',
+        'assets/js/app': 'src/ts/app.ts',
         'assets/js/otp-form': 'src/ts/otp-form.ts',
         index: 'index.html',
       },
       output: [
         {
           entryFileNames: '[name].js',
+          assetFileNames: '[name].[ext]',
         }
       ]
     },
